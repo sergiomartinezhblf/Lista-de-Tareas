1b282a25f8883d6d1eb2b85c40a7d6e7c6af5a94
Sergio Martinez <sergiomartinezhblf@hotmail.com>
2022-07-31 22:24:22 -0500

    correccion  completar agregar y elimanar tareas mas su estado

--- main.js
+++ main.js
@@ -4,44 +4,80 @@ const $template = document.getElementById("template").content;
 const $fragment = document.createDocumentFragment();
 const $btnOrdenar = document.getElementById("ordenar");
 const $contenedor = document.getElementById("contenedor-tareas");
-const db= [];
+const db= {};
 let x = 1;
 
 const genId = ()=> x++;
+
+const imprimeTarea =()=>{
+
+  Object.values(db).forEach(el => {
+    $clone = $template.cloneNode(true);
+    if(el.Estado){
+      $clone.getElementById("tarea").classList.replace('tarea','success');
+    }
+    $clone.querySelector("p").textContent= `${el.Nombre.toUpperCase()}`;
+    $clone.querySelector("img").dataset.id=el.Id;
+    $clone.querySelector("span").dataset.id=el.Id;
+    
+  $fragment.appendChild($clone);
+  });
+  $contenedor.appendChild($fragment);
+}
+
 
 document.addEventListener("submit",e =>{
    e.preventDefault(); 
    $contenedor.textContent="";
     nombreTarea = $texto.value;
-   const tarea = {
-       Id: genId(),
-       Nombre: nombreTarea,
-       Estado: false
-   };
+    const tarea = {
+      Id: genId(),
+      Nombre: nombreTarea,
+      Estado: false
+  };
 
-   console.log(tarea);
-   db.push(tarea);
-   console.log(db);
+  console.log(tarea);
+db[tarea.Id]=tarea;
+console.log(db);
    $formulario.reset();
 
-  db.forEach(el => {
-    $template.querySelector("p").textContent= `${el.Nombre.toUpperCase()}`;
-  $clone = document.importNode($template,true);
-  $fragment.appendChild($clone);
-  });
-  $contenedor.appendChild($fragment);
+  imprimeTarea();
   
 });
+
+
+
 
 const tareaCompletada = () =>{
    document.addEventListener("click", e => {
       if(e.target.classList.contains('check-icon')){
-        alert("funciona");
+        e.target.parentElement.parentElement.classList.add('success');
+        db[e.target.dataset.id].Estado=true;
+        console.log(db[e.target.dataset.id]);
       }
+      
    });
+   imprimeTarea();
 };
 
+const borrarTarea = () =>{
+  document.addEventListener("click", e => {
+    if(e.target.classList.contains('delate-icon')){      
+      e.target.parentElement.parentElement.style.display="none";
+      delete db[e.target.dataset.id];
+    }
+    
+});
+   imprimeTarea();
+};
+
+
+
+
+borrarTarea();
 tareaCompletada();
+
+
 
 
 
